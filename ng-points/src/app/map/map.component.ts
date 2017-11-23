62de7a6f8921b740d5b428377a7e779ca9f7e4fc
Marion <marion.vacherat@gmail.com>
2017-11-23 12:27:15 +0100

    close #1 display points on the map

--- ng-points/src/app/map/map.component.ts
+++ ng-points/src/app/map/map.component.ts
@@ -1,10 +1,10 @@
-import {Component, ElementRef, Input, OnInit, SimpleChange, SimpleChanges, ViewChild} from '@angular/core';
+import {Component, ElementRef, Input, OnChanges, OnInit, SimpleChange, SimpleChanges, ViewChild} from '@angular/core';
 import {WrapperService} from "../services/wrapper.service";
 import {Point} from "../../models/models";
 import {PointsService} from "../points.service";
+import {copyObj} from "@angular/animations/browser/src/util";
 
 declare const google:any;
-
 
 @Component({
   selector: 'app-map',
@@ -12,11 +12,13 @@ declare const google:any;
   styleUrls: ['./map.component.css']
 })
 
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnChanges {
 
   nativeMapElement:any;
   map:any;
   @Input() pointsFromPointsComponent:Point[]=[];
+  markers;
+  myLatLngs;
 
   constructor(public element:ElementRef, public service:PointsService) {
   }
@@ -26,8 +28,12 @@ export class MapComponent implements OnInit {
     this.initMap()
   }
 
-  ngOnChanges() {
-   this.addPoints();
+  ngOnChanges(changes: SimpleChanges) {
+   console.log("changement !", this.pointsFromPointsComponent);
+   if (this.map!=null){
+     this.initMap()
+   }
+   this.addPoints(this.pointsFromPointsComponent);
   }
 
   initMap(){
@@ -36,34 +42,34 @@ export class MapComponent implements OnInit {
       zoom: 13,
       mapTypeId: 'roadmap'
     });
-    console.log("tu as créé la carte you BMF !")
-    console.log("voici les points: "+this.pointsFromPointsComponent)
+
   }
 
-  addPoints(){
+  addPoints(points:Point[]){
     //on parcourt la liste de points et on ajoute les points à la carte
 
-    for(let point of this.pointsFromPointsComponent){
-      console.log("voici le nom du point à sa creation : "+point.nom)
-      var myLatLng = new google.maps.LatLng(point.latitude, point.longitude);
-      var marker = new google.maps.Marker({
-        position: myLatLng,
+    this.markers = [];
+    //markers.forEach(function(marker){marker.setMap(null)});
+    this.myLatLngs = [];
+    for(let i = 0 ; i<points.length;i++){
+      var point = points[i];
+
+      this.myLatLngs[i] = new google.maps.LatLng(point.latitude, point.longitude);
+      this.markers[i] = new google.maps.Marker({
+        position: this.myLatLngs[i],
         map: this.map,
         clickable: true,
         animation: google.maps.Animation.DROP, /* animation : le point est déposé sur la carte */
         descriptionLabo: point.nom, /* description qui sera affichée lorsqu'on clique sur le point */
+
       });
-      google.maps.event.addListener(marker, 'click', function () {
+      google.maps.event.addListener(this.markers[i], 'click', function () {
         /* Ajoute l'info-bulle sur le point lorsqu'on clique dessus */
         var infobulle = new google.maps.InfoWindow({
           content: this.descriptionLabo
         });
         infobulle.open(this.map, this);
       });
-
     }
-
-
-
   }
 }
